4b57c0b8a2c2129b2dd322661640ee7ac80b51a5
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-04-24 23:56:43 +0200

    Lesson 8 - Swagger

--- src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -10,8 +10,10 @@ public class Company {
 
   @ApiModelProperty(value = "Tax identification number", required = true, example = "123-123-22-22")
   private String taxIdentifications;
+
   @ApiModelProperty(value = "Company address", required = true, example = "ul. Cedrowa 16, 05-074 Hipolitów")
   private String address;
+
   @ApiModelProperty(value = "Company name", required = true, example = "Przykładowa nazwa Sp.z o.o.")
   private String name;
 
--- src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -11,12 +11,16 @@ public class InvoiceEntry {
 
   @ApiModelProperty(value = "Product/service description", required = true, example = "Lenovo z1234")
   private String description;
+
   @ApiModelProperty(value = "Number of items", required = true, example = "33")
   private int quantity;
+
   @ApiModelProperty(value = "Product/service netto price", required = true, example = "1111.22")
   private BigDecimal price;
+
   @ApiModelProperty(value = "Product/ service tax value", required = true, example = "22.22")
   private BigDecimal vatValue;
+
   @ApiModelProperty(value = "tax rate", required = true)
   private Vat vatRate;
 
