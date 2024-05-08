07aa13d50f20600aa71d2139cf24bb1ff8d21891
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-05-08 22:49:11 +0200

    Lesson 9 - Tax Calculator

--- src/main/java/pl/futurecollars/invoicing/InvoiceApplication.java
+++ src/main/java/pl/futurecollars/invoicing/InvoiceApplication.java
@@ -1,12 +1,25 @@
 package pl.futurecollars.invoicing;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InvoiceApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     SpringApplication.run(InvoiceApplication.class, args);
+
+    InputStream inputStream = new FileInputStream("xyz.json");
+    Reader reader = new InputStreamReader(inputStream);
+
+    InputStream inputStream2 = new FileInputStream("xyz2.json");
+    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream2);
+
   }
 }
--- src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,15 +1,19 @@
 package pl.futurecollars.invoicing.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Company {
 
   @ApiModelProperty(value = "Tax identification number", required = true, example = "123-123-22-22")
-  private String taxIdentifications;
+  private String taxIdentificationNumber;
 
   @ApiModelProperty(value = "Company address", required = true, example = "ul. Cedrowa 16, 05-074 Hipolitów")
   private String address;
@@ -17,9 +21,4 @@ public class Company {
   @ApiModelProperty(value = "Company name", required = true, example = "Przykładowa nazwa Sp.z o.o.")
   private String name;
 
-  public Company(String taxIdentifications, String address, String name) {
-    this.taxIdentifications = taxIdentifications;
-    this.address = address;
-    this.name = name;
-  }
 }
