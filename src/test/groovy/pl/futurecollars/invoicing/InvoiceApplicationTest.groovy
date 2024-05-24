551c3b3386a0c8232dea0673da804fc0f1028693
TAbramczyk1126 <155571869+TAbramczyk1126@users.noreply.github.com>
2024-05-24 11:56:29 +0200

    Lesson 14 - noSQL & MongoDb (#15)
    * Lesson 14 - noSQL & MongoDb
    
    * Lesson 14 - noSQL & MongoDb
    
    * Lesson 14 - noSQL & MongoDb
    
    * Lesson 14 - noSQL & MongoDb
    
    ---------
    
    Co-authored-by: T.Abramczyk1126 <t.abramczyk@gmail.com>

--- src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
@@ -2,7 +2,6 @@ package pl.futurecollars.invoicing
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import pl.futurecollars.invoicing.model.Invoice
 import pl.futurecollars.invoicing.service.InvoiceService
 import spock.lang.Specification
 
@@ -12,20 +11,9 @@ class InvoiceApplicationTest extends Specification {
     @Autowired
     private InvoiceService invoiceService;
 
-    def "invoice service is created"() {
-        expect:
-        invoiceService
+        def "invoice service is created"() {
+            expect:
+            invoiceService
 
+        }
     }
-
-    def "should return no invoices when database is empty"() {
-        given:
-
-        when:
-        List<Invoice> invoices = invoiceService.getAll()
-
-        then:
-        invoices.isEmpty()
-        invoices.size() == 0
-    }
-}
